86f3ba931b4b53d7eeba7a65f096425e76e2c4ba
Italo Silva de Assunção <83298919+italosassuncao@users.noreply.github.com>
2023-01-12 15:29:59 -0300

    Update AlunoDAO.java

--- src/main/java/br/edu/unicid/dao/AlunoDAO.java
+++ src/main/java/br/edu/unicid/dao/AlunoDAO.java
@@ -15,48 +15,42 @@ public class AlunoDAO {
 	private Aluno aluno;
 
 	public AlunoDAO() throws Exception {
-		// chama a Classe ConnectionFacotry
+		// chama a classe ConnectionFactory e estabele uma conexão
 		try {
 			this.conn = ConnectionFactory.getConnection();
-		} catch (Exception erro) {
-			throw new Exception(erro.getMessage());
+		} catch (Exception e) {
+			throw new Exception("erro: \n" + e.getMessage());
 		}
 	}
 
-	// método salvar
+	// método de salvar
 	public void salvar(Aluno aluno) throws Exception {
-		if (aluno == null) {
-			throw new Exception("Preencha as informações do aluno");
-		}
+		if (aluno == null)
+			throw new Exception("O valor passado não pode ser nulo");
 		try {
-			String SQL = "INSERT INTO tbAluno(caAluno, nomeAluno, emailAluno, dtaNasc, endAluno, idadeAluno) values (?, ?, ?, ? ,? ,?)";
+			String SQL = "INSERT INTO tbAluno (caAluno, nomeAluno, emailAluno, dtaNasc, endAluno, idadeAluno) values (?, ?, ?, ?, ?, ?)";
 			conn = this.conn;
 			ps = conn.prepareStatement(SQL);
 			ps.setInt(1, aluno.getCaAluno());
 			ps.setString(2, aluno.getNomeAluno());
 			ps.setString(3, aluno.getEmailAluno());
-			ps.setDate(4, new java.sql.Date(aluno.getDtaNasc().getTime()));
+			ps.setDate(4, new java.sql.Date(aluno.getDtaNasc().getTime()));		
 			ps.setString(5, aluno.getEndAluno());
 			ps.setInt(6, aluno.getIdadeAluno());
 			ps.executeUpdate();
-
-		} catch (SQLException sql) {
-
-			throw new Exception("Erro ao inserir os dados. " + sql);
-
+		} catch (SQLException sqle) {
+			throw new Exception("Erro ao inserir dados " + sqle);
 		} finally {
-			ConnectionFactory.closeConnection(conn, ps, rs);
+			ConnectionFactory.closeConnection(conn, ps);
 		}
 	}
 
-	// método atualizar
+	// método de atualizar
 	public void atualizar(Aluno aluno) throws Exception {
-		if (aluno == null) {
-			throw new Exception("Preencha as informações do aluno");
-		}
+		if (aluno == null)
+			throw new Exception("O valor passado nao pode ser nulo");
 		try {
-			String SQL = "UPDATE tbAluno set nomeAluno=?, emailAluno=?, dtaNasc=?,"
-					+ " endAluno=?, idadeAluno=? WHERE caAluno=?";
+			String SQL = "UPDATE tbAluno set nomeAluno=?, emailAluno=?, dtaNasc=?, endAluno=?, idadeAluno=? WHERE caAluno = ?";
 			conn = this.conn;
 			ps = conn.prepareStatement(SQL);
 			ps.setString(1, aluno.getNomeAluno());
@@ -66,88 +60,74 @@ public class AlunoDAO {
 			ps.setInt(5, aluno.getIdadeAluno());
 			ps.setInt(6, aluno.getCaAluno());
 			ps.executeUpdate();
-
-		} catch (SQLException sql) {
-
-			throw new Exception("Erro ao inserir os dados. " + sql);
-
+		} catch (SQLException sqle) {
+			throw new Exception("Erro ao alterar dados " + sqle);
 		} finally {
-			ConnectionFactory.closeConnection(conn, ps, rs);
+			ConnectionFactory.closeConnection(conn, ps);
 		}
 	}
 
-	// método excluir
+	// método de excluir
 	public void excluir(Aluno aluno) throws Exception {
-		if (aluno == null) {
-			throw new Exception("Preencha as informações do aluno");
-		}
+		if (aluno == null)
+			throw new Exception("O valor passado nao pode ser nulo");
 		try {
-			String SQL = "DELETE FROM tbAluno WHERE caAluno=?";
+			String SQL = "DELETE FROM tbAluno WHERE caAluno = ?";
 			conn = this.conn;
 			ps = conn.prepareStatement(SQL);
 			ps.setInt(1, aluno.getCaAluno());
 			ps.executeUpdate();
-
-		} catch (SQLException sql) {
-
-			throw new Exception("Erro ao inserir os dados. " + sql);
-
+		} catch (SQLException sqle) {
+			throw new Exception("Erro ao excluir dados " + sqle);
 		} finally {
-			ConnectionFactory.closeConnection(conn, ps, rs);
+			ConnectionFactory.closeConnection(conn, ps);
 		}
 	}
 
-	// método listar
-	public List todosAlunos() throws Exception {
-		try {
-			String SQL = "SELECT * FROM tbAluno";
-			conn = this.conn;
-			ps = conn.prepareStatement(SQL);
-			rs = ps.executeQuery();
-
-			List<Aluno> list = new ArrayList<Aluno>();
-
-			while (rs.next()) {
-				int ca = rs.getInt(1);
-				String nome = rs.getString(2);
-				String email = rs.getString(3);
-				Date nascimento = rs.getDate(4);
-				String endereco = rs.getString(5);
-				int idade = rs.getInt(6);
-
-				list.add(new Aluno(ca, nome, email, nascimento, endereco, idade));
-			}
-
-			return list;
-
-		} catch (SQLException sql) {
-
-			throw new Exception("Erro ao inserir os dados. " + sql);
-
-		} finally {
-			ConnectionFactory.closeConnection(conn, ps, rs);
-		}
-	}
-	
-	// método buscar
+	// procurar aluno
 	public Aluno procurarAluno(int caAluno) throws Exception {
 
 		try {
 			String SQL = "SELECT  * FROM tbAluno WHERE caAluno=?";
 			conn = this.conn;
 			ps = conn.prepareStatement(SQL);
-			ps.setInt(1, caAluno);			
+			ps.setInt(1, caAluno);
 			rs = ps.executeQuery();
 			if (rs.next()) {
 				int ca = rs.getInt(1);
 				String nome = rs.getString(2);
 				String email = rs.getString(3);
-				Date nascimento = rs.getDate(4);
-				int idade = rs.getInt(5);
-				String endereco = rs.getString(6);
+				Date nascimento = rs.getDate(4);				
+				String endereco = rs.getString(5);
+				int idade = rs.getInt(6);
 				aluno = new Aluno(ca, nome, email, nascimento, endereco, idade);
 			}
 			return aluno;
+		} catch (SQLException sqle) {
+			throw new Exception(sqle);
+		} finally {
+			ConnectionFactory.closeConnection(conn, ps, rs);
+		}
+	}
+
+	// listar todos os alunos
+	public List todosAlunos() throws Exception {
+		try {
+			String SQL = "SELECT  * FROM tbAluno";
+			conn = this.conn;
+			ps = conn.prepareStatement(SQL);
+			rs = ps.executeQuery();
+			List<Aluno> list = new ArrayList<Aluno>();
+			while (rs.next()) {
+				int ca = rs.getInt(1);
+				String nome = rs.getString(2);
+				String email = rs.getString(3);
+				Date nascimento = rs.getDate(4);				
+				String endereco = rs.getString(5);
+				int idade = rs.getInt(6);
+				list.add(new Aluno(ca, nome, email, nascimento, endereco, idade));
+			}
+			return list;
 		} catch (SQLException sqle) {
 			throw new Exception(sqle);
 		} finally {
